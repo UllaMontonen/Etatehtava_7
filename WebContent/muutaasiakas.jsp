eb24a9802f4e3f9eec0148270a3cc1b49d3c04e0
Montonen Ulla <bgq363@myy.haaga-helia.fi>
2022-05-05 19:03:50 +0300

    Added possibility to change existing customer information

---
+++ WebContent/muutaasiakas.jsp
@@ -0,0 +1,117 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<script src="scripts/main.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.15.0/jquery.validate.min.js"></script>
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<title>Muuta asiakas</title>
+</head>
+<body>
+<form id="tiedot">
+	<table>
+		<thead>	
+			<tr>
+				<th colspan="5" class="oikealle"><span id="takaisin">Takaisin listaukseen</span></th>
+			</tr>		
+			<tr>
+				<th>Etunimi</th>
+				<th>Sukunimi</th>
+				<th>Puhelin</th>
+				<th>Sposti</th>	
+				<th></th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td><input type="text" name="etunimi" id="etunimi"></td>
+				<td><input type="text" name="sukunimi" id="sukunimi"></td>
+				<td><input type="text" name="puhelin" id="puhelin"></td>
+				<td><input type="text" name="sposti" id="sposti"></td> 
+				<td><input type="submit" id="tallenna" value="Muuta"></td>
+			</tr>
+		</tbody>
+	</table>
+</form>
+<span id="ilmo"></span>
+</body>
+<script>
+$(document).ready(function(){
+	$("#takaisin").click(function(){
+		document.location="listaaasiakkaat.jsp";
+	});
+	//Haetaan muutettavan asiakkaan tiedot. Kutsutaan backin GET-metodia ja välitetään kutsun mukana muutettavan tiedon id
+	//GET /asiakkaat/haeyksi/asiakas_id
+	var asiakas_id = requestURLParam("asiakas_id"); //Funktio löytyy scripts/main.js 	
+	$.ajax({url:"asiakkaat/haeyksi/"+asiakas_id, type:"GET", dataType:"json", success:function(result){	
+		$("#asiakas_id").val(result.asiakas_id);		
+		$("#etunimi").val(result.etunimi);	
+		$("#sukunimi").val(result.sukunimi);
+		$("#puhelin").val(result.puhelin);
+		$("#sposti").val(result.sposti);			
+    }});
+	
+	$("#tiedot").validate({						
+		rules: {
+			etunimi:  {
+				required: true,
+				minlength: 1				
+			},	
+			sukunimi:  {
+				required: true,
+				minlength: 1				
+			},
+			puhelin:  {
+				required: true,
+				number: true,
+				minlength: 6
+			},	
+			sposti:  {
+				required: true,
+				minlength: 4,
+			}	
+		},
+		messages: {
+			etunimi: {     
+				required: "Puuttuu",
+				minlength: "Liian lyhyt"			
+			},
+			sukunimi: {
+				required: "Puuttuu",
+				minlength: "Liian lyhyt"
+			},
+			puhelin: {
+				required: "Puuttuu",
+				number: "Ei kelpaa",
+				minlength: "Liian lyhyt"
+			},
+			sposti: {
+				required: "Puuttuu",
+				minlength: "Liian lyhyt",
+			}
+		},			
+		submitHandler: function(form) {	
+			paivitaTiedot();
+		}		
+	}); 	
+	//viedään kursori etunimi kohtaan sivun latauksen yhteydessä
+	$("#etunimi").focus(); 
+});
+//funktio tietojen lisäämistä varten. Kutsutaan backin POST-metodia ja välitetään kutsun mukana uudet tiedot json-stringinä.
+//POST /asiakkaat/
+function paivitaTiedot(){	
+	var formJsonStr = formDataJsonStr($("#tiedot").serializeArray()); //muutetaan lomakkeen tiedot json-stringiksi
+	$.ajax({url:"asiakkaat", data:formJsonStr, type:"PUT", dataType:"json", success:function(result) { //result on joko {"response:1"} tai {"response:0"}       
+		if(result.response==0){
+      	$("#ilmo").html("Asiakkaan päivittäminen epäonnistui.");
+      }else if(result.response==1){			
+      	$("#ilmo").html("Asiakkaan päivittäminen onnistui.");
+      	$("#etunimi", "#sukunimi", "#puhelin", "#sposti").val("");
+		}
+  }});	
+}
+</script>
+</html>
--- src/control/Asiakkaat.java
+++ src/control/Asiakkaat.java
@@ -12,6 +12,7 @@ import org.json.JSONObject;
 import model.Asiakas;
 import model.dao.Dao;
 
+//REST-metodeja asiakastietojen hallintaan
 @WebServlet("/asiakkaat/*")
 public class Asiakkaat extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -21,23 +22,42 @@ public class Asiakkaat extends HttpServlet {
         System.out.println("Asiakkaat.Asiakkaat()");
     }
 	// Asiakkaiden hakeminen
+    //GET  /asiakkaat/{hakusana}
+    //GET /asiakkaat/haeyksi/asiakas_id
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Asiakkaat.doGet()");
 		String pathInfo = request.getPathInfo();	//haetaan kutsun polkutiedot, esim. /aalto			
 		System.out.println("polku: "+pathInfo);	
-		String hakusana="";
-		if(pathInfo!=null) {		
-			hakusana = pathInfo.replace("/", "");
-		}		
 		Dao dao = new Dao();
-		ArrayList<Asiakas> asiakkaat = dao.listaaKaikki(hakusana);
-		System.out.println(asiakkaat);
-		String strJSON = new JSONObject().put("asiakkaat", asiakkaat).toString();	
+		ArrayList<Asiakas> asiakkaat;
+		String strJSON="";
+		if(pathInfo==null) { //Haetaan kaikki autot 
+			asiakkaat = dao.listaaKaikki();
+			strJSON = new JSONObject().put("asiakkaat", asiakkaat).toString();	
+		}else if(pathInfo.indexOf("haeyksi")!=-1) {		//polussa on sana "haeyksi", eli haetaan yhden asiakkaan tiedot
+			int asiakas_id = Integer.parseInt(pathInfo.replace("/haeyksi/", "")); //poistetaan polusta "/haeyksi/", j‰ljelle j‰‰ asiakas_id		
+			Asiakas asiakas = dao.etsiAsiakas(asiakas_id);
+			if(asiakas==null) {
+				strJSON = "{}";
+			} else {
+			JSONObject JSON = new JSONObject();
+			JSON.put("etunimi", asiakas.getEtunimi());
+			JSON.put("sukunimi", asiakas.getSukunimi());
+			JSON.put("puhelin", asiakas.getPuhelin());
+			JSON.put("sposti", asiakas.getSposti());	
+			strJSON = JSON.toString();		
+			}
+		}else{ //Haetaan hakusanan mukaiset autot
+			String hakusana = pathInfo.replace("/", "");
+			asiakkaat = dao.listaaKaikki(hakusana);
+			strJSON = new JSONObject().put("asiakkaat", asiakkaat).toString();	
+		}	
 		response.setContentType("application/json");
 		PrintWriter out = response.getWriter();
 		out.println(strJSON);		
 	}
 	// Asiakkaan lis‰‰minen
+	//POST / asiakkaat
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Asiakkaat.doPost()");
 		JSONObject jsonObj = new JsonStrToObj().convert(request); //Muutetaan kutsun mukana tuleva json-string json-objektiksi			
@@ -56,8 +76,24 @@ public class Asiakkaat extends HttpServlet {
 		}		
 	}
 	// Asiakkaan muuttaminen
+	// PUT / asiakkaat
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Asiakkaat.doPut()");		
+		System.out.println("Asiakkaat.doPut()");	
+		JSONObject jsonObj = new JsonStrToObj().convert(request); //Muutetaan kutsun mukana tuleva json-string json-objektiksi		
+		int asiakas_id = jsonObj.getInt("asiakas_id");
+		Asiakas asiakas = new Asiakas();
+		asiakas.setEtunimi(jsonObj.getString("etunimi"));
+		asiakas.setSukunimi(jsonObj.getString("sukunimi"));
+		asiakas.setPuhelin(jsonObj.getString("puhelin"));
+		asiakas.setSposti(jsonObj.getString("sposti"));
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		Dao dao = new Dao();			
+		if(dao.muutaAsiakas(asiakas, asiakas_id)){ //metodi palauttaa true/false
+			out.println("{\"response\":1}");  //Asiakkaan p‰ivitt‰minen onnistui {"response":1}
+		}else{
+			out.println("{\"response\":0}");  //Asiakkaan p‰ivitt‰minen ep‰onnistui {"response":0}
+		}	
 	}
 	// Asiakkaan poistaminen
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
